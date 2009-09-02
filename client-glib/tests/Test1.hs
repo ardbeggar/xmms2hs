c214a287bb3c03363d1336735ec17173147db071
Oleg Belozeorov <upwawet@gmail.com>
2009-09-02 14:25:50 +0600

    * rewrote XMMS2 Tutorial 8 in Haskell

--- client-glib/tests/Test1.hs
+++ client-glib/tests/Test1.hs
@@ -4,13 +4,25 @@ module Main
 import qualified XMMS2.Client.Connection as X
 import XMMS2.Client.Result
 import XMMS2.Client.Playback
+import XMMS2.Client.Value
 import XMMS2.Client.Glib
+import System.Glib.MainLoop
 
 main = do
+  ml <- mainLoopNew Nothing False
   (Just c) <- X.init "test1"
-  r <- X.connect c Nothing
-  start c >>= wait
-  print r
+  True     <- X.connect c Nothing
+  stop c
+  start c
+  r <- signalPlaybackPlaytime c
+  notifierSet r $ myPlayTime ml
+  mainLoopGMainInit c
+  mainLoopRun ml
+
+myPlayTime ml v = do
+  (Just t) <- getInt v
+  print t
+  if t > 5000 then (print "done" >> mainLoopQuit ml >> return False) else return True
 
 -- local variables:
 -- compile-command: "ghc --make Test1.hs"
