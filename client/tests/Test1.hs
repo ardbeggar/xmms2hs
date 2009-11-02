641dad1e685b43c24c734aa4cd102fb0d573de9d
Oleg Belozeorov <upwawet@gmail.com>
2009-11-02 20:02:35 +0500

    * vrious tests

--- client/tests/Test1.hs
+++ client/tests/Test1.hs
@@ -5,7 +5,9 @@ import qualified XMMS2.Client.Connection as X
 import XMMS2.Client.Result
 import XMMS2.Client.Playback
 import XMMS2.Client.Medialib
-import XMMS2.Client.Value  
+import XMMS2.Client.Value
+import Data.Map (Map)
+import qualified Data.Map as Map
 
 main = do
   let url = "file:///srv/share/music/Absu/Tara '2001/01 Tara.flac"
@@ -17,7 +19,17 @@ main = do
   resultWait r
   v <- resultGetValue r
   getInt v >>= print
+  testMaps
+
+
+testMap1 k = Map.fromList $ map (\n -> (show n, DataInt32 n)) [k .. k + 10]
+testMap2 = Map.fromList $ map (\n -> (show n, testMap1 n)) [0 .. 5]
+
+testMaps = do
+  val <- valueNew testMap2
+  map :: Dict (Dict ValueData) <- valueGet val
+  print $ map == testMap2
 
 -- local variables:
--- compile-command: "ghc --make Test1.hs"
+-- compile-command: "ghc -XScopedTypeVariables --make Test1.hs"
 -- end:
