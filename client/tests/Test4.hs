9c9a0432597e9fcc19a907c1a8e41053ed90e4ff
Oleg Belozeorov <upwawet@gmail.com>
2010-02-13 23:34:45 +0500

    * updated tests

--- client/tests/Test4.hs
+++ client/tests/Test4.hs
@@ -4,11 +4,11 @@ module Main
 import qualified XMMS2.Client.Connection as X
 import XMMS2.Client.Monad
 import XMMS2.Client.Monad.XMMS
-import XMMS2.Client.Glib  
+import XMMS2.Client.Glib
 import System.Glib.MainLoop
 import Control.Monad.Trans
-  
-  
+
+
 main = do
   mloop <- mainLoopNew Nothing False
   (Just c) <- X.init "test4"
@@ -16,14 +16,14 @@ main = do
   mainLoopGMainInit c
   runXMMS (playlistListEntries Nothing >>* printEntries mloop) c ()
   mainLoopRun mloop
-             
+
 printEntries mloop = do
   entries <- result
   liftIO $ mapM_ print $ (take 5 entries)
-  liftIO $ mapM_ print $ (take 7 entries)
+  liftIO $ mapM_ print $ (take 10 entries)
   liftIO $ mainLoopQuit mloop
   return False
-              
+
 
 -- local variables:
 -- compile-command: "ghc --make Test4.hs"
